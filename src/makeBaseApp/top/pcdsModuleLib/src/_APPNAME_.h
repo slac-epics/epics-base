3fe9f27bb41f79d91ddad42e48657ab7ec236f28
Bruce Hill <bhill@slac.stanford.edu>
2011-01-03 02:37:28 +0000

    Added pcds templates for makeBaseApp
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/branches/merge/epics/base/current@4661 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ src/makeBaseApp/top/pcdsModuleLib/src/_APPNAME_.h
@@ -0,0 +1,29 @@
+#ifndef	_APPNAME__H
+#define _APPNAME__H
+
+/*
+ *	Author:  _USER_
+ *
+ *	Abstract:
+ *		TODO:	What does this lib provide?
+ */
+
+#ifdef	__cplusplus
+extern "C" {
+#endif	/*	__cplusplus	*/
+
+
+/*
+ * Constants
+ */
+
+
+/*
+ * Function declarations
+ */
+int		_APPNAME_Init( );
+
+#ifdef	__cplusplus
+}
+#endif	/*	__cplusplus	*/
+#endif	/*	_APPNAME_H	*/
