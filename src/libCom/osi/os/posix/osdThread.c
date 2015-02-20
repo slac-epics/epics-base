50423ced208a2ef0e2bebb8a296029233457d377
Andrew Johnson <anj@aps.anl.gov>
2015-02-20 12:25:28 -0600

    Applied linux-runtime-disable-RT-priorities-if-not-supported.patch

--- src/libCom/osi/os/posix/osdThread.c
+++ src/libCom/osi/os/posix/osdThread.c
@@ -67,6 +67,7 @@ typedef struct commonAttr{
     int                maxPriority;
     int                minPriority;
     int                schedPolicy;
+    int                usePolicy;
 } commonAttr;
 
 typedef struct epicsThreadOSD {
@@ -92,6 +93,7 @@ typedef struct epicsThreadOSD {
 typedef struct {
     int min_pri, max_pri;
     int policy;
+    int ok;
 } priAvailable;
 #endif
 
@@ -150,6 +152,8 @@ static void setSchedulingPolicy(epicsThreadOSD *pthreadInfo,int policy)
 {
 #if defined (_POSIX_THREAD_PRIORITY_SCHEDULING)
     int status;
+
+    if(!pcommonAttr->usePolicy) return;
 
     status = pthread_attr_getschedparam(
         &pthreadInfo->attr,&pthreadInfo->schedParam);
@@ -291,6 +295,7 @@ int           low, try;
 
     prm->min_pri = min;
     prm->max_pri = try_pri(max, prm->policy) ? max-1 : max;
+    prm->ok = 1;
 
     return 0;
 }
@@ -303,6 +308,7 @@ void         *dummy;
 int          status;
 
     arg.policy = a_p->schedPolicy;
+    arg.ok = 0;
 
     status = pthread_create(&id, 0, find_pri_range, &arg);
     checkStatusQuit(status, "pthread_create","epicsThreadInit");
@@ -312,6 +318,7 @@ int          status;
 
     a_p->minPriority = arg.min_pri;
     a_p->maxPriority = arg.max_pri;
+    a_p->usePolicy = arg.ok;
 }
 #endif
 
@@ -644,6 +651,7 @@ epicsShareFunc void epicsShareAPI epicsThreadSetPriority(epicsThreadId pthreadIn
     pthreadInfo->osiPriority = priority;
     if(!pthreadInfo->isFifoScheduled) return;
 #if defined (_POSIX_THREAD_PRIORITY_SCHEDULING) 
+    if(!pcommonAttr->usePolicy) return;
     pthreadInfo->schedParam.sched_priority = getOssPriorityValue(pthreadInfo);
     status = pthread_attr_setschedparam(
         &pthreadInfo->attr,&pthreadInfo->schedParam);
