936ffe24ca6a2ff968423c251e65623a3d58cac1
Bruce Hill <bhill@slac.stanford.edu>
2016-11-03 22:06:44 -0700

    Merging R3.4.12.5

--- src/rsrv/camessage.c
+++ src/rsrv/camessage.c
@@ -278,17 +278,14 @@ static void log_header (
             hostName, pContext);
     }
 
-    epicsPrintf (
-"CAS: Request from %s => cmmd=%d cid=0x%x type=%d count=%d postsize=%u\n",
+    epicsPrintf ( "CAS: Request from %s => cmmd=%d cid=0x%x type=%d count=%d postsize=%u\n",
         hostName, mp->m_cmmd, mp->m_cid, mp->m_dataType, mp->m_count, mp->m_postsize);
 
-    epicsPrintf (   
-"CAS: Request from %s =>  available=0x%x \tN=%u paddr=%p\n",
-        hostName, mp->m_available, mnum, (pciu?(void *)&pciu->addr:NULL));
+    epicsPrintf ( "CAS: Request from %s =>   available=0x%x \tN=%u paddr=%p\n",
+        hostName, mp->m_available, mnum, (pciu ? (void *)&pciu->addr : NULL));
 
     if (mp->m_cmmd==CA_PROTO_WRITE && mp->m_dataType==DBF_STRING && pPayLoad ) {
-        epicsPrintf (
-"CAS: Request from %s => \tThe string written: %s \n",
+        epicsPrintf ( "CAS: Request from %s =>   Wrote string \"%s\"\n",
         hostName, (char *)pPayLoad );
     }
 }
