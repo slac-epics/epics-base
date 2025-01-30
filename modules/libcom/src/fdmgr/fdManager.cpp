3f9e618be82fbdb92d7e887d1cfa3584ae29b248
Dirk Zimoch <dirk.zimoch@psi.ch>
2025-01-30 12:01:48 +0100

    fix codacy warning: make fdManagerPrivate constructor explicit

--- modules/libcom/src/fdmgr/fdManager.cpp
+++ modules/libcom/src/fdmgr/fdManager.cpp
@@ -76,7 +76,7 @@ struct fdManagerPrivate {
     fdReg * pCBReg;
     fdManager & owner;
 
-    fdManagerPrivate(fdManager & owner);
+    explicit fdManagerPrivate(fdManager & owner);
     void lazyInitTimerQueue();
 };
 
