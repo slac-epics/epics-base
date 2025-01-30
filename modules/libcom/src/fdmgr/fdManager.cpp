f854b964b4e93169d0d7e7e3bfce24b24f76258f
Dirk Zimoch <dirk.zimoch@psi.ch>
2025-01-30 16:25:29 +0100

    fdManager: filter poll flags for Window's sake

--- modules/libcom/src/fdmgr/fdManager.cpp
+++ modules/libcom/src/fdmgr/fdManager.cpp
@@ -36,9 +36,7 @@
 
 #ifdef FDMGR_USE_POLL
 #include <vector>
-#if defined(_WIN32)
-#define poll WSAPoll
-#else
+#if !defined(_WIN32)
 #include <poll.h>
 #endif
 
@@ -46,6 +44,15 @@ static const short PollEvents[] = { // must match fdRegType
     POLLRDNORM | POLLRDBAND | POLLIN | POLLHUP | POLLERR,
     POLLWRBAND | POLLWRNORM | POLLOUT | POLLERR,
     POLLPRI};
+
+#if defined(_WIN32)
+#define poll WSAPoll
+// Filter out PollEvents that Windows does not accept in events (only returns in revents)
+#define WIN_POLLEVENT_FILTER(ev) static_cast<short>((ev) & (POLLIN | POLLOUT))
+#else
+// Linux, MacOS and RTEMS don't care
+#define WIN_POLLEVENT_FILTER(ev) (ev)
+#endif
 #endif
 
 #ifdef FDMGR_USE_SELECT
@@ -102,7 +109,9 @@ epicsTimer & fdManager::createTimer()
 fdManager fileDescriptorManager;
 
 static const unsigned mSecPerSec = 1000u;
+#ifdef FDMGR_USE_SELECT
 static const unsigned uSecPerSec = 1000u * mSecPerSec;
+#endif
 
 //
 // fdManager::fdManager()
@@ -180,11 +189,14 @@ epicsShareFunc void fdManager::process (double delay)
 
 #ifdef FDMGR_USE_POLL
 #if __cplusplus >= 201100L
-        priv->pollfds.emplace_back(pollfd{.fd = iter->getFD(), .events = PollEvents[iter->getType()]});
+        priv->pollfds.emplace_back(pollfd{
+            .fd = iter->getFD(),
+            .events = WIN_POLLEVENT_FILTER(PollEvents[iter->getType()])
+        });
 #else
         struct pollfd pollfd;
         pollfd.fd = iter->getFD();
-        pollfd.events = PollEvents[iter->getType()];
+        pollfd.events = WIN_POLLEVENT_FILTER(PollEvents[iter->getType()]);
         pollfd.revents = 0;
         priv->pollfds.push_back(pollfd);
 #endif
@@ -235,7 +247,7 @@ epicsShareFunc void fdManager::process (double delay)
                 // But just in case...
                 int isave = i;
                 while (priv->pollfds[i].fd != iter->getFD() ||
-                    priv->pollfds[i].events != PollEvents[iter->getType()])
+                    priv->pollfds[i].events != WIN_POLLEVENT_FILTER(PollEvents[iter->getType()]))
                 {
                     errlogPrintf("fdManager: skipping (removed?) pollfd %d (expected %d)\n", priv->pollfds[i].fd, iter->getFD());
                     i++; // skip pollfd of removed items
